d00a3b35a982377d4343b9e4b91542a34b15b73a
unknown <mancini.domenico456@gmail.com>
2020-09-21 11:04:40 +0200

    First real commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+const body = document.querySelector('body');
+const navbar = document.querySelector('nav');
+const hamburgerIcon = document.querySelector('.menu-icon i');
+const list = document.querySelector('nav ul');
+
+document.addEventListener('DOMContentLoaded', () => {
+    hamburgerIcon.addEventListener('click', () => {
+        list.classList.toggle('showing');
+    });
+});
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY != 0)
+        navbar.classList.add('black');
+    else navbar.classList.remove('black');
+});
+
+function removePreloader() {
+    body.classList.remove('no-overflow');
+    const preloader = document.querySelector('.preloader');
+    preloader.classList.add('fadeOut');
+    setTimeout(() => { preloader.remove(); }, 1000);
+}
+
+window.addEventListener('load', () => {
+    removePreloader();
+});
