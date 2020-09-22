5c5fa71decec05e15caf58198a6d7e7563a8ef11
unknown <mancini.domenico456@gmail.com>
2020-09-22 17:06:58 +0200

    Light and Dark mode.

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ const body = document.querySelector('body');
 const navbar = document.querySelector('nav');
 const hamburgerIcon = document.querySelector('.menu-icon i');
 const list = document.querySelector('nav ul');
+const lightToggle = document.querySelector('#light-toggle input[type="checkbox"]');
+
+let lightOn = false;
 
 document.addEventListener('DOMContentLoaded', () => {
     hamburgerIcon.addEventListener('click', () => {
@@ -15,6 +18,21 @@ window.addEventListener('scroll', () => {
     else navbar.classList.remove('opaque');
 });
 
+function updateTheme() {
+    document.documentElement.setAttribute("data-theme", lightOn ? "light" : "dark");
+}
+
+function selectDefaultTheme() {
+    lightOn = window.matchMedia('(prefers-color-scheme: dark)').matches ? false : true;
+    lightToggle.checked = lightOn;
+    updateTheme();
+}
+
+function toggleLight() {
+    lightOn = !lightOn;
+    updateTheme();
+}
+
 function removePreloader() {
     body.classList.remove('no-overflow');
     const preloader = document.querySelector('.preloader');
@@ -23,5 +41,7 @@ function removePreloader() {
 }
 
 window.addEventListener('load', () => {
+    selectDefaultTheme();
+    lightToggle.addEventListener('change', toggleLight);
     removePreloader();
 });
